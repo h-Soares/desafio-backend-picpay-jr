6432d50e5230b450760ee6790fbfa4b5ec126009
h-Soares <hiagosoares002@gmail.com>
2025-04-13 15:05:44 -0300

    Logs added to external API services

--- src/main/java/com/soaresdev/picpaytestjr/v1/services/AuthorizationService.java
+++ src/main/java/com/soaresdev/picpaytestjr/v1/services/AuthorizationService.java
@@ -2,6 +2,8 @@ package com.soaresdev.picpaytestjr.v1.services;
 
 import com.soaresdev.picpaytestjr.exceptions.TransferException;
 import com.soaresdev.picpaytestjr.v1.dtos.externalApisDto.authorize.AuthorizeDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -9,6 +11,7 @@ import java.util.Objects;
 
 @Service
 public class AuthorizationService {
+    private final Logger logger = LoggerFactory.getLogger(AuthorizationService.class.getName());
     private static final String URL = "https://util.devi.tools/api/v2/authorize";
     private final RestClient restClient;
 
@@ -17,6 +20,7 @@ public class AuthorizationService {
     }
 
     public boolean isAuthorized() {
+        logger.info("Checking if authorized via external API call to {}...", URL);
         return Objects.requireNonNull(restClient.get().
                 retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
--- src/main/java/com/soaresdev/picpaytestjr/v1/services/NotificationService.java
+++ src/main/java/com/soaresdev/picpaytestjr/v1/services/NotificationService.java
@@ -1,10 +1,13 @@
 package com.soaresdev.picpaytestjr.v1.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Service
 public class NotificationService {
+    private final Logger logger = LoggerFactory.getLogger(NotificationService.class.getName());
     private static final String URL = "https://util.devi.tools/api/v1/notify";
     private final RestClient restClient;
 
@@ -13,6 +16,7 @@ public class NotificationService {
     }
 
     public void notifyUser() {
+        logger.info("Sending user notification via external API call to {}...", URL);
         restClient.post().retrieve().toBodilessEntity();
     }
 }
